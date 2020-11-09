f3028f732bb96356f759ea6f1e1efa140abfcb5a
Kevin Tang <kev123361@gmail.com>
2020-11-09 13:43:15 -0500

    First prototype

---
+++ dist/DrawManager.js
@@ -0,0 +1,44 @@
+var socket;
+var port;
+
+var canvas;
+
+function setup() {
+   
+    socket = io.connect('http://localhost:3000');
+
+    socket.on("newDrawingData", ReceiveNewDrawing);
+    setupCanvas();
+
+
+}
+
+function setupCanvas() {
+    canvas = new fabric.Canvas('canvas', {
+        isDrawingMode:true
+    });
+    //canvas.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 100, left: 100}));
+
+    canvas.on('path:created', function(e) {
+        //console.log(e);
+        var newPath = e.path;
+        console.log(newPath);
+        var data = {
+            drawing: newPath
+        };
+        socket.emit("newDrawing", data);
+    });
+
+}
+
+function ReceiveNewDrawing(data) {
+    console.log(data.drawing);
+    var path = new fabric.Path(data.drawing.path);
+    path.set( {
+        fill: data.drawing.fill,
+        stroke: data.drawing.stroke
+    });
+    //path.path = data.drawing.path;
+    console.log(path);
+    canvas.add(path);
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+
+//Starting the server
+var express = require('express');
+var app = express();
+var server = app.listen(process.env.PORT || 3000); //we can see the website at localhost:4000 when server is running
+app.use(express.static('dist')); //everything in this folder is now available at localhost:4000
+console.log("My socket server is running");
+var socket = require('socket.io');
+var io = socket(server);
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    var thisID;
+    console.log('new connection: ' + socket.id);
+
+    socket.on("newDrawing", sendNewDrawings);
+
+    function sendNewDrawings(data) {
+        console.log("Got Here");
+        //console.log(data.drawing);
+        socket.broadcast.emit("newDrawingData", data);
+    }
+}
+
+
+
